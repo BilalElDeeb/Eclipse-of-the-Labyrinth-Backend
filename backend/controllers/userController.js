73d22f11eb869a32867b3ef5467cada2f72d28f1
BilalElDeeb <BE2201234@tkh.edu.eg>
2025-05-01 13:05:23 +0300

    Created the user routes
    Created the user routes and noticed an error, the id was used to delete the user not the username and since i dont think we will be sending in id i updated it

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,10 +51,10 @@ const saveDungeonDifficulty = async (req, res) => {
 };
 
 const deleteUser = async (req, res) => {
-    const { id } = req.params;
+    const { username } = req.params;
   
     try {
-      const user = await User.findByIdAndDelete(id);
+      const user = await User.findOneAndDelete({ username });
       if (!user) return res.status(404).json({ error: "User not found" });
   
       res.status(200).json({ message: "User deleted" });
