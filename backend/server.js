0b01e8d547cf69f3a3356bdaa97a082c99e05b42
BilalElDeeb <BE2201234@tkh.edu.eg>
2025-05-01 13:06:53 +0300

    created server.js

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/databaseConnection");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+app.use("/api/user", userRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
